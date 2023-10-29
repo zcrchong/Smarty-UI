cc022a4040c7cca55b890e381e6480b079ec8418
chaoranzuo <15269876632@163.com>
2023-10-29 17:54:55 +0800

    fix: 回滚

--- src/entry.ts
+++ src/entry.ts
@@ -3,8 +3,13 @@ import MyButton from "./Button";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
 
+
 // 导出单独组件
 export { MyButton, SFCButton, JSXButton };
+
+
+
+
 
 // 编写一个插件，实现一个install方法
 
